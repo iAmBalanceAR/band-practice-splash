0c9068c2b9c4340d17d2a86adb65de2020aca492
iAmBalanceAR <arkansasdj@gmail.com>
2025-01-24 13:53:55 -0600

    removed links to live site till launch 2

--- components/splash/cta-section.tsx
+++ components/splash/cta-section.tsx
@@ -1,8 +1,8 @@
 "use client"
 
 import { motion } from 'framer-motion'
-import { Button } from "@/components/ui/button"
-import Link from 'next/link'
+// import { Button } from "@/components/ui/button"
+// import Link from 'next/link'
 
 export default function CTASection() {
   return (
